466a0897947d32c67dd1ab6d1772464461b6d361
Jitender Singh Virk <jitendersinghvirk47@gmail.com>
2018-07-18 15:36:17 +0530

    adding output images

--- watershed.py
+++ watershed.py
@@ -56,6 +56,8 @@ for i, imgpath in enumerate(imagepaths):
     # stacked2d = np.hstack((gray, opening))
     stacked3d = np.hstack((bgrimg, bgrimgo))
 
+    cv2.imwrite("output{}.png".format(i), stacked3d)
+
     # cv2.imshow("filters", stacked2d)
     cv2.imshow("finals", stacked3d)
 
